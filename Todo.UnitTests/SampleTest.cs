e85a1422aa840cc8d7d209e1af26c617208ca60b
clayton.hunt <clayton.hunt@e-hps.com>
2014-08-01 11:35:23 -0400

    added sample test that should fail

---
+++ Todo.UnitTests/SampleTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Todo.UnitTests
+{
+    [TestClass]
+    public class SampleTest
+    {
+        [TestMethod]
+        public void Sample()
+        {
+            var a = 1;
+            Assert.AreEqual(2, a);
+        }
+    }
+}
