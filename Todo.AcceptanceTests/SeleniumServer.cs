bfb966b489c0bc98af8878e0bb6d911f822d888e
clayton.hunt <clayton.hunt@e-hps.com>
2014-08-01 10:07:57 -0400

    Initial commit with single selenium test

---
+++ Todo.AcceptanceTests/SeleniumServer.cs
@@ -0,0 +1,40 @@
+﻿using System.Configuration;
+using System.Diagnostics;
+
+namespace Todo.AcceptanceTests
+{
+    public class SeleniumServer
+    {
+        private static Process _process;
+
+        public static bool IsStarted { get; set; }
+
+        private static readonly string JavaPath = ConfigurationManager.AppSettings["java"];
+        private static readonly string JavaArguments = ConfigurationManager.AppSettings["selenium"];
+
+        public static void Start()
+        {
+            if (IsStarted)
+                return;
+
+            IsStarted = true;
+
+            _process = new Process {
+                StartInfo = {
+                    FileName = JavaPath, 
+                    Arguments = JavaArguments, 
+                    CreateNoWindow = false
+                }
+            };
+
+            _process.Start();
+        }
+
+        public static void Stop()
+        {
+            _process.Kill();
+
+            IsStarted = false;
+        }
+    }
+}
